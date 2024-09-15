b70787030ec25feaccdac4f82633e333c007064b
giorgi786 <omanadzegiorgi1@gmail.com>
2024-09-15 22:20:48 +0400

    commit

---
+++ day 02/classwork/task01.py
@@ -0,0 +1,33 @@
+# ცლადი ინახავს მნიშვნელობას
+# ცვლადის სახელი, = ცლადს ენიჭება მნიშნელობა, ცვლადის მინიჭება
+# = ცვლადს ენიწება მნიშვნელობა, --- declaration / დეკლარაცია
+name = "საათი"
+# ცვლადის სახელით ხდება მისი მნიშვნელობის დაწერა
+# ცვლადის სახელის გამოძახებით
+# name
+# statements --- მოქმედება
+# ცვლადის სახელები არ დაიწყოთ დიდი ასოებით სიმბოლოებით
+name = " "
+# contry
+#snake_case, camelCase
+heart_amouts = 14
+print(heart_amouts)
+print("--------------------")
+
+population = 2500
+# type() ---  რა მონემთა ტიპი გადმოგვეცემა
+# numeric --- მათემატიკური ოპერაციების განხორციელება 
+# მათემატიკური ოპერაციები არის მიმატება, გამოკლება...
+
+# NUMERIC DATA VS TEXTUAL DATA
+# NUMERIC DATA
+print("--------------------------")
+# -, +, *, **, /, //, %
+# population = 2500
+print(population - 500)
+print(population + 500)
+print(population * 500)
+print(population ** 1)
+print(population / 500) # / float           
+print(population // 500) # // int 
+print("საათი " + "საათი")
---
+++ day 02/homework/task01.py
@@ -0,0 +1,29 @@
+population = 1000
+hamburger = 200
+french_fries = 150
+lines =  2000
+words = 500
+money = 450
+phones =  210
+keyboard = 5
+mouse = 50
+laptop = 15
+
+name = "giorgi"
+last_name = "omanadze"
+age = 15
+hobby = "vollyball"
+country = "tbilisi"
+print(name + " "   + last_name + " "  + str(age) + " " + hobby + " " + country )
+
+population = 666
+print(population - 333)
+print(population + 444)
+print(population * 4)
+print(population / 30 )
+print(population // 30)
+print(population  ** 2)
+print(population  - 200)
+print(population  + 400)
+print(population  // 2)
+print(population  * 20)
